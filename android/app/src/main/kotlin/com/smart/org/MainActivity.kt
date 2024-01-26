54e239e81d44031edf4dd17f66ec4da1203a69ba
Matthias Steinkamp <matthias.steinkamp@tu-dortmund.de>
2024-01-26 19:35:47 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/smart/org/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smart.org.template_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:template_app/app/app.dart';
+import 'package:template_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
